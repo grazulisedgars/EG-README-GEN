67a3ff19dd28e8e8a732e537a5781ae7106925e5
grazulisedgars <grazulisedgars@gmail.com>
2023-12-15 13:53:48 +0200

    Adds array of questions for user

--- index.js
+++ index.js
@@ -5,7 +5,36 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Description",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Installation",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Usage Information",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Contribution Guidelines",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Test Instructions",
+    }
 ];
 
 // function to write README file
