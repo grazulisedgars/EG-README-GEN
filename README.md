9c26464132cb217b965cfdca581d9364e4af2a4a
grazulisedgars <grazulisedgars@gmail.com>
2023-12-15 14:29:18 +0200

    Modifies generateMarkdown.js

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# EG-README-GEN
+# EG-README
+
+  ## Description
+  asdasdasdasd
+
+  ## Table of Contents
+  - [Installation] (#installation)
+  - [Usage] (#usage)
+  - [Licence] (#licence)
+  - [Contributing] (#contribution)
+  - [Tests] (#tests)
+  - [Questions] (#questions)
+
+  ## Installation
+  dfghgfdfgh
+
+  ## Usage
+  sdfbnfdsdafg
+
+  ## Licence
+  undefined
+
+  ## Contributing
+  sadfgnfdsadf
+
+  ## Tests 
+  undefined
+
+  ## Questions
+  Github: [undefined] (https://github.com/undefined)
+  Email: undefined
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,36 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation] (#installation)
+  - [Usage] (#usage)
+  - [Licence] (#licence)
+  - [Contributing] (#contribution)
+  - [Tests] (#tests)
+  - [Questions] (#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Licence
+  ${data.licence}
+
+  ## Contributing
+  ${data.contribution}
+
+  ## Tests 
+  ${data.tests}
+
+  ## Questions
+  Github: [${data.username}] (https://github.com/${data.username})
+  Email: ${data.email}
+
 `;
 }
 
